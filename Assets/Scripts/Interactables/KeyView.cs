9c2773c6fc4a9c8c24a19cca90aed52ed32f6e1d
abhi6403 <163630847+abhi6403@users.noreply.github.com>
2025-02-17 18:58:49 +0530

    Implemented Generic Event Controller

--- Assets/Scripts/Interactables/KeyView.cs
+++ Assets/Scripts/Interactables/KeyView.cs
@@ -5,6 +5,9 @@ public class KeyView : MonoBehaviour, IInteractable
     [SerializeField] GameUIView gameUIView;
     public void Interact()
     {
+
+        int currentKeys = GameService.Instance.GetPlayerController().KeysEquipped;
+        
         GameService.Instance.GetInstructionView().HideInstruction();
         GameService.Instance.GetSoundView().PlaySoundEffects(SoundType.KeyPickUp);
         GameService.Instance.GetPlayerController().KeysEquipped++;
